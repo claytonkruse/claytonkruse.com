a677d8163c724178d1cad09f9efc61964476ec34
Hiemswolf <claytonkruse08@gmail.com>
2024-02-06 01:11:40 -0600

    second commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import './style.css';
+</script>
+
+<header class="h-10"><div></div></header>
+<main id="main" class="m-auto max-w-2xl">
+	<slot />
+</main>
+<footer class="px-8">
+	<small>Clayton Kruse, Computer Science Student at Mizzou</small>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,131 @@
+<h1 class="font-serif">Clayton Kruse</h1>
+<p>
+	Hello, I am a full time CS student at Mizzou, who skipped CS 1050 for CS 2050
+	in my first year, and I am looking for summer internship opportunities.
+</p>
+<h2>Contact Me</h2>
+<table>
+	<tr>
+		<td>Email</td>
+		<td>
+			<a href="mailto:claytonkruse08@gmail.com">claytonkruse08@gmail.com</a>
+		</td>
+	</tr>
+	<tr>
+		<td>Phone</td>
+		<td>
+			<a href="tel:+13147452624">(314) 745-2624</a>
+		</td>
+	</tr>
+</table>
+
+<!-- <h2>Technologies I have Experience With</h2> -->
+<h2>Education</h2>
+<p>In high school I took a number of classes related to computer science.</p>
+<ul>
+	<li>
+		Classes Taken in High School
+		<ul>
+			<li>Intro to Programming</li>
+			<li>
+				AP Computer Science
+				<ul>
+					<li>OOP & Inheritence</li>
+				</ul>
+			</li>
+			<li>
+				Web Developement
+				<ul>
+					<li>Learned PHP</li>
+					<li>Learned SQL with <a href="https://www.mysql.com/">MySQL</a></li>
+				</ul>
+			</li>
+			<li>
+				Networking
+				<ul><li>Windows Network Administration</li></ul>
+			</li>
+		</ul>
+	</li>
+	<li>
+		Awarded for Highest Innovation at Missouri S&T Summer Pre-Decision Visit
+	</li>
+</ul>
+<h3>Univeristy Education at Mizzou</h3>
+<p>
+	On my first day of class at Mizzou, I walked into CS 1050, sat down, and was
+	bewildered by how basic the material was. After the class ended, I immediately
+	contacted my Academic Advisor and asked him about potentially skipping the
+	class for something more advanced. He informed me of a policy that would allow
+	me to skip, and get credit for CS 1050, so long as I could get above a B in
+	the more advanced class, CS 2050. It's now the spring semester, and I have
+	successfully recieved credit for both CS 1050 and 2050 with a cumulative GPA
+	of 3.44 for the fall semester.
+</p>
+<h2>Related Projects and Hobbies</h2>
+<p>
+	I started programing games on <a href="https://scratch.mit.edu">Scratch</a> in
+	elementary school. Inspired to program at an early age, I continued on to create
+	numerous projects on the platform that far exceeded Scratch's scope. I did all
+	of my programming in Scratch until I moved on in middle school.
+</p>
+<p>
+	In middle school, I spent a lot of time working with JavaScript and Node.JS.
+	First, I made a basic realtime updating chat application with <a
+		href="https://socket.io/">Socket.IO</a
+	>
+	and <a href="https://expressjs.com/">Express</a>. I contiuned on to make an
+	online multiplayer browser game using the same libraries, hosted on
+	<a href="https://www.heroku.com/">Heroku</a>. At some point during middle
+	school I was also running a small flash games website that was used by many
+	students in the school. I also began hosting a Minecraft server that is still
+	online to this day.
+</p>
+<p>
+	In high school, I created a Discord bot via <a href="https://discord.js.org/"
+		>Discord.JS</a
+	>. This bot also included a frontend written in
+	<a href="https://react.dev/">React</a> and had persistent functionality that I
+	achieved with <a href="https://www.mongodb.com/">MongoDB</a>. I also wrote
+	HTML proxies from scratch, before I realized there are easier solutions to
+	bypass a blocklist.
+</p>
+<p>
+	I also made the swtich from Windows to Unix like operating systems in high
+	school. I fiddled around with different Linux distrobutions until I settled on <a
+		href="https://archlinux.org/">Arch</a
+	>. I also run some Linux boxes at home, which I use for
+	<a href="https://jellyfin.org/">Jellyfin</a> and as a NAS.
+</p>
+<p>
+	In college, so far I have been working on multiple web applications in <a
+		href="https://kit.svelte.dev/">SvelteKit</a
+	>. These applications have
+	<a href="https://www.sqlite.org/index.html">SQLite</a>
+	and <a href="https://www.postgresql.org/">PostgreSQL</a> databases that I
+	interact with through <a href="https://www.prisma.io/">Prisma</a>. These
+	applications also include authentication functionality that I initally wrote
+	myself; however, I have migrated to
+	<a href="https://lucia-auth.com/">Lucia</a>.
+</p>
+
+<h2>Experience as an Employee</h2>
+<p>I have been formally employed by buisnesses on two seperate occasions.</p>
+<p>
+	My first job was working part time at Dairy Queen in the back, which was known
+	as the grill. I prepared all the hot food at that location. My reponsiblietes
+	included managing a grill, multiple fryers, multiple microwaves, two ovens,
+	and the freezer. I always worked the closing shift, so at the end of the
+	night, I would do all the dishes, clean the grill and the fryers, take the
+	grease out back behind the dumpster and pour it into the receptical, clean all
+	surfaces including the insides of the microwaves, take out the trash, and
+	finally mop the floors.
+</p>
+<p>
+	My second job was working full time at Dierbergs in the Deli department. This
+	job was much the same as working at Dairy Queen. Both jobs had lots of hard
+	work preparing food, cleaning, and working the closing shift. The difference
+	with Dierbergs was that I was now working full time, and in a position where I
+	had to both provide customer service, and functionally work as a team with my
+	co-workers, which was in stark contrast to the solitude of Dairy Queen. During
+	my time at Dierbergs, I was also in charge of training new part-time hires.
+</p>
