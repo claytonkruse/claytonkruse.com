6ac6d4759a1d3ff8db3ee16b32293b878f426ac5
Hiemswolf <claytonkruse08@gmail.com>
2024-09-05 00:35:51 -0500

    add repo link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,10 @@
 		<slot />
 	</main>
 	<footer class="py-3 text-center">
-		<small class="font-serif">Clayton Kruse, Developer</small>
+		<small class="font-serif"
+			>Clayton Kruse, Developer | View the source code for this website on <a
+				href="https://github.com/claytonkruse/claytonkruse.com">GitHub</a
+			>.</small
+		>
 	</footer>
 </div>
