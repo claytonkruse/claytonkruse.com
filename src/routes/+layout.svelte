bec11a090a6618fbb5e53abec81bf4da7c86dcab
Hiemswolf <claytonkruse08@gmail.com>
2024-10-29 19:25:13 -0500

    migrate to svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
 	import Header from './Header.svelte';
 	import './style.css';
+	/**
+	 * @typedef {Object} Props
+	 * @property {import('svelte').Snippet} [children]
+	 */
+
+	/** @type {Props} */
+	let { children } = $props();
 </script>
 
 <div class="flex min-h-full flex-col justify-between">
 	<Header></Header>
 	<main id="main" class="m-auto max-w-3xl p-7">
-		<slot />
+		{@render children?.()}
 	</main>
 	<footer class="px-7 py-3 text-center">
 		<small class="font-serif"
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,23 +1,27 @@
+<!-- @migration-task Error while migrating Svelte code: `<tr>` is invalid inside `<table>` -->
+<!-- @migration-task Error while migrating Svelte code: `<tr>` is invalid inside `<table>` -->
 <svelte:head>
 	<title>Contact Me | Clayton Kruse</title>
 </svelte:head>
 
 <h1 class="mb-1 text-lg uppercase">Contact Me</h1>
 <table>
-	<tr>
-		<td>Email</td>
-		<td>
-			<a href="mailto:ck@claytonkruse.com">ck@clayk.cc</a>
-		</td>
-	</tr>
-	<tr>
-		<td>Phone</td>
-		<td>
-			<a href="tel:+13147452624">(314) 745-2624</a>
-		</td>
-	</tr>
-	<tr>
-		<td>LinkedIn</td>
-		<td><a href="https://www.linkedin.com/in/claykruse/">clayk.cc/in</a></td>
-	</tr>
+	<tbody>
+		<tr>
+			<td>Email</td>
+			<td>
+				<a href="mailto:ck@claytonkruse.com">ck@clayk.cc</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Phone</td>
+			<td>
+				<a href="tel:+13147452624">(314) 745-2624</a>
+			</td>
+		</tr>
+		<tr>
+			<td>LinkedIn</td>
+			<td><a href="https://www.linkedin.com/in/claykruse/">clayk.cc/in</a></td>
+		</tr>
+	</tbody>
 </table>
