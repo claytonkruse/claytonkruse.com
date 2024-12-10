f77f602bd2f3299973d88b9ad2ef291bfe4dbea1
Clayton Kruse <claytonkruse08@gmail.com>
2024-12-10 15:58:18 -0600

    change home button

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,20 +1,20 @@
 <header
-	class="justify-center border-b-[1px] border-b-zinc-800 px-7 py-2 font-serif uppercase md:flex md:px-0"
+	class="justify-center border-b-[1px] border-b-zinc-800 font-serif uppercase md:flex md:px-0"
 >
-	<nav class="left-5 md:absolute">
-		<ul class="flex list-none p-0">
-			<li><a class="font-bold" href="/">Clayton Kruse</a></li>
-		</ul>
+	<nav class="left-5 h-full md:absolute">
+		<a href="/">
+			<img src="/favicon.png" alt="Home" class="w-10 align-baseline" />
+		</a>
 	</nav>
 	<nav>
-		<ul class="flex list-none gap-3 p-0">
+		<ul>
 			<li><a href="/resume/">Resume</a></li>
 			<li><a href="/work-history/">Work History</a></li>
 			<li><a href="/contact/">Contact</a></li>
 		</ul>
 	</nav>
 	<nav class="right-5 md:absolute">
-		<ul class="flex list-none gap-3 p-0">
+		<ul>
 			<li><a href="/friends/">Friends</a></li>
 			<li><a href="/articles/">Articles</a></li>
 			<li><a href="/projects/">Projects</a></li>
@@ -23,10 +23,13 @@
 </header>
 
 <style lang="postcss">
+	ul {
+		@apply flex list-none gap-3 p-0;
+	}
+	li a {
+		@apply block text-nowrap py-2;
+	}
 	header a {
 		@apply text-zinc-100;
-	}
-	li {
-		@apply text-nowrap;
 	}
 </style>
